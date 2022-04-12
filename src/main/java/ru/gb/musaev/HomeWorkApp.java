9cc3010e441923e0f3b36fefc6a0751eda7fe48c
user <suymonkul1999@gmail.com>
2022-04-13 02:04:16 +0300

    Third task

--- src/main/java/ru/gb/musaev/HomeWorkApp.java
+++ src/main/java/ru/gb/musaev/HomeWorkApp.java
@@ -3,6 +3,7 @@ package ru.gb.musaev;
 public class HomeWorkApp  {
      public static void main (String[] args){
          printThreeWords();
+         checkSumSign();
     }
 
     public static void printThreeWords() {
@@ -10,6 +11,17 @@ public class HomeWorkApp  {
         System.out.println("Banana");
         System.out.println("Apple");
      }
+
+
+    public static void checkSumSign() {
+     int a=8, b=6;
+     int c=a+b;
+     if (c>=20){
+         System.out.println("Сумма положительная");
+     } else {
+         System.out.println("Сумма отрицательная");
+     }
+    }
 
 }
 
