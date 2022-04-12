7a9a80d36878529f4e40cbc2e1d512ce9124f52c
user <suymonkul1999@gmail.com>
2022-04-13 00:15:45 +0300

    printThreeWords  commit

---
+++ src/main/java/ru/gb/musaev/HomeWorkApp.java
@@ -0,0 +1,14 @@
+package ru.gb.musaev;
+
+public class HomeWorkApp  {
+     public static void main (String[] args){
+         printThreeWords();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+     }
+}
+
