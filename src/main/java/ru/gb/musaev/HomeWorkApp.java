53fa83b39968f71e4fd2fe6dde60f75d246df786
user <suymonkul1999@gmail.com>
2022-04-13 01:00:42 +0300

    First and second task

--- src/main/java/ru/gb/musaev/HomeWorkApp.java
+++ src/main/java/ru/gb/musaev/HomeWorkApp.java
@@ -5,10 +5,11 @@ public class HomeWorkApp  {
          printThreeWords();
     }
 
-    private static void printThreeWords() {
+    public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
      }
+
 }
 
