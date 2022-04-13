d63197703f4db21955200bfeca1c9cd41fdd1d79
user <suymonkul1999@gmail.com>
2022-04-13 23:07:32 +0300

    fifth task

--- src/main/java/ru/gb/musaev/HomeWorkApp.java
+++ src/main/java/ru/gb/musaev/HomeWorkApp.java
@@ -4,7 +4,12 @@ public class HomeWorkApp  {
      public static void main (String[] args){
          printThreeWords();
          checkSumSign();
+         printColor();
+         compareNumbers();
+
     }
+
+
 
     public static void printThreeWords() {
         System.out.println("Orange");
@@ -14,14 +19,35 @@ public class HomeWorkApp  {
 
 
     public static void checkSumSign() {
-     int a=8, b=6;
-     int c=a+b;
-     if (c>=20){
+     int a = 8, b = 6;
+     int c = a + b;
+     if (c >= 0){
          System.out.println("Сумма положительная");
      } else {
          System.out.println("Сумма отрицательная");
      }
     }
+    public static void printColor(){
+       int value = 5;
+       if (value < 0){
+           System.out.println("Красный");
+       }
+       else if (value > 0 && value <= 100){
+           System.out.println("Желтый");
+       }
+
+     }
+    private static void compareNumbers() {
+         int  a = 20, b = 25;
+         if (a >= b){
+             System.out.println("a >= b");
+         }
+         else{
+             System.out.println("“a < b”");
+         }
+    }
+
+
 
 }
 
