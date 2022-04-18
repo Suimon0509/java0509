804a575f8ccc1f9c74078ae966c41818496c6348
user <suymonkul1999@gmail.com>
2022-04-18 14:24:39 +0300

    first task (2 homework)

--- src/main/java/ru/gb/musaev/HomeWorkSecond.java
+++ src/main/java/ru/gb/musaev/HomeWorkSecond.java
@@ -1,22 +1,21 @@
-package ru.gb.musaev;
+    package ru.gb.musaev;
 
-public class HomeWorkSecond {
-    public static void main(String[] args) {
-        myFirstMethod();
+    public class HomeWorkSecond {
+        public static void main(String[] args) {
+            myFirstMethod();
+
+        }
+
+        public static int myFirstMethod() {
+            int a = 6, b = 9;
+            int c = a + b;
+            boolean i = (c >= 10 && c <= 20);
+            System.out.println(i);
+            return c;
+        }
+
 
 
 
     }
-
-    private static int myFirstMethod() {
-        int a = 6, b = 9;
-        int c = a + b;
-        boolean i = (c >= 10 && c <= 20);
-        System.out.println(i);
-        return c;
-    }
-
-
-
-}
 
