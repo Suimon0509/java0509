267d1ce30c485879bb9cdabd45cadc3bdf4e2740
user <suymonkul1999@gmail.com>
2022-04-19 20:58:32 +0300

    fourth task (2 homework)

--- src/main/java/ru/gb/musaev/HomeWorkSecond.java
+++ src/main/java/ru/gb/musaev/HomeWorkSecond.java
@@ -5,7 +5,7 @@
             myFirstMethod();
             secondTesk(-5);
             thirdMethodTesk(5);
-
+            myFourthHomeWork();
         }
 
         public static int myFirstMethod() {
@@ -15,20 +15,34 @@
             System.out.println(i);
             return c;
         }
-        public static void  secondTesk(int a) {
-            if (a <= 0){
+
+        public static void secondTesk(int a) {
+            if (a <= 0) {
                 System.out.println("Отрицательное");
             } else {
                 System.out.println("Положительное");
             }
         }
-        public static int thirdMethodTesk(int a){
-            boolean i =  (a <= 0);
+
+        public static int thirdMethodTesk(int a) {
+            boolean i = (a <= 0);
             System.out.println(i);
             return a;
+        }
+        public static void myFourthHomeWork() {
+
+            for (int i = 0; i < 6; i++) System.out.println("GeekBrains");
+        }
+
+
         }
 
 
 
-    }
+
+
+
+
+
+
 
