65b0fe186a4d934b83782f863e551a22692e9449
user <suymonkul1999@gmail.com>
2022-05-14 23:53:10 +0300

    third task(5 homework)

--- src/main/java/ru/gb/musaev/leasson5/Employee.java
+++ src/main/java/ru/gb/musaev/leasson5/Employee.java
@@ -5,16 +5,26 @@ public class Employee {
     private String position;
     private String email;
     private String phone;
-    private int salary;
+    private double salary;
     private int age;
 
-    public Employee(fullname, position, email, phone, salary, age) {
+    public Employee(String fullname, String position,String  email,String  phone, double salary,int age) {
         this.fullname = fullname;
         this.position = position;
         this.email = email;
         this.phone = phone;
         this.salary = salary;
         this.age = age;
+    }
+    public void info(){
+        System.out.println("ФИО:" + fullname + " ," +
+                            "Должность:" + position + " ," +
+                            "Email:" + email + " ," +
+                            "Зарплата:" + salary + " ," +
+                            "Возраст:" + age  );
+    }
+    public int getage(){
+        return age;
     }
 }
 
--- src/main/java/ru/gb/musaev/leasson5/Leasson5_OOPS.java
+++ src/main/java/ru/gb/musaev/leasson5/Leasson5_OOPS.java
@@ -3,5 +3,6 @@ package ru.gb.musaev.leasson5;
 public class Leasson5_OOPS {
     public static void main(String[] args){
 
+
     }
 }
